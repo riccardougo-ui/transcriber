bfa3c1bffbe3f3f2e36ce5307b4a0f9e27425cf4
riccardougo-ui <riccardougo@outlook.com>
2026-01-13 23:01:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log("Server avviato su porta", PORT);
 });
 
